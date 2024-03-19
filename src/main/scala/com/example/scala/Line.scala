8743ebf7af4490ced24a9b83d465b884e4a21b76
LEGIONLARS\Lars- <lars-pedersen5539@hotmail.com>
2024-03-19 11:46:19 +0100

    Adding DrawRect to the Line-class

--- src/main/scala/com/example/scala/Line.scala
+++ src/main/scala/com/example/scala/Line.scala
@@ -63,94 +63,114 @@ end if
 import scala.collection.JavaConverters.seqAsJavaListConverter
 import com.example.scala.Pixel
 
+import scala.::
 import scala.collection.mutable.ListBuffer
 
 final class CustomLine {
 
 
-private def plotLineHigh(x0:Int, y0:Int, x1:Int, y1:Int) : List[Pixel] = {
-  var pixels: ListBuffer[Pixel] = ListBuffer[Pixel]()
-  var dx = x1 - x0
-  val dy = y1 - y0
-  var xi = 1
+  private def plotLineHigh(x0: Int, y0: Int, x1: Int, y1: Int): List[Pixel] = {
+    var pixels: ListBuffer[Pixel] = ListBuffer[Pixel]()
+    var dx = x1 - x0
+    val dy = y1 - y0
+    var xi = 1
 
-  if (dx < 0) {
-    xi = -1
-    dx = -dx
-  }
-
-  var D = (2*dx)-dy
-  var x = x0
-
-
-
-  for (y <- y0 to y1){
-    pixels += new Pixel(x,y)
-
-    if(D > 0){
-      x = x + xi
-      D = D + (2 * (dx - dy))
-    }
-    else{
-      D = D + 2*dx
+    if (dx < 0) {
+      xi = -1
+      dx = -dx
     }
 
-  }
-
-  return pixels.toList
-}
-
-private def plotLineLow(x0: Int, y0: Int, x1: Int, y1: Int): List[Pixel] = {
-  var pixels: ListBuffer[Pixel] = ListBuffer[Pixel]()
-  val dx = x1 - x0
-  var dy = y1 - y0
-  var yi = 1
-
-  if (dy < 0) {
-    yi = -1
-    dy = -dy
-  }
-
-  var D = (2 * dy) - dx
-  var y = y0
+    var D = (2 * dx) - dy
+    var x = x0
 
 
+    for (y <- y0 to y1) {
+      pixels += new Pixel(x, y)
 
-  for (x <- x0 to x1){
-    pixels += new Pixel(x,y)
-
-    if(D>0){
-      y = y + yi
-      D = D + (2 * (dy - dx))
-    }
-    else{
-      D = D + 2*dy
-    }
-
-  }
-
-  return pixels.toList
-}
-
-
-
-
-def drawLine(x0:Int, y0:Int, x1:Int, y1:Int ) : java.util.List[Pixel]  = {
-
-  if( (y1-y0).abs < (x1-x0).abs  ) {
-    if (x0 > x1){
-        return plotLineLow(x1,y1,x0,y0).asJava
+      if (D > 0) {
+        x = x + xi
+        D = D + (2 * (dx - dy))
       }
-    else return plotLineLow(x0,y0,x1,y1).asJava
-  }
-  else
-  {
-    if (y0 > y1) {
-      return plotLineHigh(x1,y1,x0,y0).asJava
-    }
-    else return plotLineHigh(x0,y0,x1,y1).asJava
+      else {
+        D = D + 2 * dx
+      }
 
+    }
+
+    return pixels.toList
   }
-}
+
+  private def plotLineLow(x0: Int, y0: Int, x1: Int, y1: Int): List[Pixel] = {
+    var pixels: ListBuffer[Pixel] = ListBuffer[Pixel]()
+    val dx = x1 - x0
+    var dy = y1 - y0
+    var yi = 1
+
+    if (dy < 0) {
+      yi = -1
+      dy = -dy
+    }
+
+    var D = (2 * dy) - dx
+    var y = y0
+
+
+    for (x <- x0 to x1) {
+      pixels += new Pixel(x, y)
+
+      if (D > 0) {
+        y = y + yi
+        D = D + (2 * (dy - dx))
+      }
+      else {
+        D = D + 2 * dy
+      }
+
+    }
+
+    return pixels.toList
+  }
+
+
+  def drawLine(x0: Int, y0: Int, x1: Int, y1: Int): java.util.List[Pixel] = {
+    return drawLine_priv(x0, y0, x1, y1).asJava
+  }
+
+  private def drawLine_priv(x0: Int, y0: Int, x1: Int, y1: Int): List[Pixel] = {
+
+    if ((y1 - y0).abs < (x1 - x0).abs) {
+      if (x0 > x1) {
+        return plotLineLow(x1, y1, x0, y0)
+      }
+      else return plotLineLow(x0, y0, x1, y1)
+    }
+    else {
+      if (y0 > y1) {
+        return plotLineHigh(x1, y1, x0, y0)
+      }
+      else return plotLineHigh(x0, y0, x1, y1)
+
+    }
+  }
+
+
+  def drawRect(x0: Int, y0: Int, x1: Int, y1: Int): java.util.List[Pixel] = {
+    // defining the 4 corners of the rectangle:
+    val corner_bl = new Pixel(x0, y0)
+    val corner_tl = new Pixel(x0, y1)
+    val corner_br = new Pixel(x1, y0)
+    val corner_tr = new Pixel(x1, y1)
+
+    // Lines between every corner:
+    val bl_to_br_pixels = drawLine_priv(corner_bl.Get_X(), corner_bl.Get_Y(), corner_br.Get_X(), corner_br.Get_Y())
+    val bl_to_tl_pixels = drawLine_priv(corner_bl.Get_X(), corner_bl.Get_Y(), corner_tl.Get_X(), corner_tl.Get_Y())
+    val tr_to_tl_pixels = drawLine_priv(corner_tr.Get_X(), corner_tr.Get_Y(), corner_tl.Get_X(), corner_tl.Get_Y())
+    val tr_to_br_pixels = drawLine_priv(corner_tr.Get_X(), corner_tr.Get_Y(), corner_br.Get_X(), corner_br.Get_Y())
+
+    val total_pixels: List[Pixel] = bl_to_br_pixels ::: bl_to_tl_pixels ::: tr_to_tl_pixels ::: tr_to_br_pixels
+
+    // Return total amount of pixels.
+    return total_pixels.asJava
+  }
 
 }
